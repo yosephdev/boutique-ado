7771658d568f7009a60785a923e60bcec59e6608
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-06-09 08:55:25 +0000

    Added checkout success logic

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
