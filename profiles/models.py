0d5990ce8e888b9277fcdb0798069df1b450658e
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-06-14 09:54:11 +0000

    Added profile form, views and updated template

--- profiles/models.py
+++ profiles/models.py
@@ -16,12 +16,12 @@ class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label="Country *", null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label="Country", null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -35,7 +35,7 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
     else:
-        if hasattr(instance, 'userprofile'):  
+        if hasattr(instance, "userprofile"):
             instance.userprofile.save()
         else:
-            UserProfile.objects.create(user=instance) 
+            UserProfile.objects.create(user=instance)
