46dceb0a1aa9f690b4760d7732110d9adc2d893b
Yoseph Berhane Gebremedhin <yosephbet@gmail.com>
2024-06-16 17:56:57 +0000

    Improved Django settings configuration

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -178,6 +178,13 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 if "USE_AWS" in os.environ:
+
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
+        "CacheControl": "max-age=94608000",
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = "boutique-adey"
     AWS_S3_REGION_NAME = "eu-central-1"
@@ -203,3 +210,22 @@ STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
 STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
 DEFAULT_FROM_EMAIL = "boutiqueado@example.com"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
